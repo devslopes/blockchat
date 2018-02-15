95548a1c47f367e5153da21335582408fa3c978c
Mark Price <devslopes@marks-mbp.lan>
2018-02-14 20:13:25 -0800

    added stuart

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
      * IP Addresses for each validator node
      */
     peers: [
-        '159.65.168.34'
+        '159.65.168.34', // Devslopes
+        '198.199.65.48'  // Stuart
     ]
 }
